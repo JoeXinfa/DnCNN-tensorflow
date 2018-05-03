a9791c0662a4dd59eb9eba075b0c9ad3fb656acc
Xinfa Zhu <zhuu@chevron.com>
2018-05-02 21:19:24 -0500

    Replace Supervisor with MonitoredTrainingSession
    Because the tf.train.Supervisor is deprecated.

--- dncnn/cluster_train.py
+++ dncnn/cluster_train.py
@@ -1,64 +1,47 @@
-import tensorflow as tf
+# -*- coding: utf-8 -*-
+
+""" Distributed TensorFlow """
+
 import argparse
+import os
 import sys
 import time
-import os
+
 import numpy as np
-# from tensorflow.contrib.session_bundle import exporter
+import tensorflow as tf
 
 from dncnn.model import dncnn
 from dncnn.utils import tf_psnr
 
 FLAGS = None
 
-def create_done_queue(i, worker_count):
-  """Queue used to signal death for i'th ps shard. Intended to have
-  all workers enqueue an item onto it to signal doneness."""
-  with tf.device("/job:ps/task:%d" % (i)):
-    return tf.FIFOQueue(worker_count, tf.int32,
-                        shared_name="done_queue"+str(i))
-
-def create_done_queues(ps_count, worker_count):
-  return [create_done_queue(i, worker_count) for i in range(ps_count)]
-
-
 def main(_):
 
     ps_hosts = FLAGS.ps_hosts.split(',')
     worker_hosts = FLAGS.worker_hosts.split(',')
+    task_index = FLAGS.task_index
+    job_name = FLAGS.job_name
 
-    # ps_count = len(ps_hosts)
-    worker_count = len(worker_hosts)
-
+    # Create a cluster from the parameter server and worker hosts.
     cluster = tf.train.ClusterSpec({"ps":ps_hosts, "worker":worker_hosts})
 
-    # start a server for a specific task
-    server = tf.train.Server(
-        cluster,
-        job_name=FLAGS.job_name,
-        task_index=FLAGS.task_index)
+    # Create and start a server for the local task.
+    server = tf.train.Server(cluster, job_name=job_name,
+                             task_index=task_index)
 
-    if FLAGS.job_name == "ps":
-        sess = tf.Session(server.target)
-        queue = create_done_queue(FLAGS.task_index, worker_count)
-
-        # wait until all workers are done
-        for i in range(worker_count):
-            sess.run(queue.dequeue())
-            print("ps %d received done worker %d" % (FLAGS.task_index, i))
-        print("ps %d: quitting" % (FLAGS.task_index))
-
-    elif FLAGS.job_name == "worker":
+    if job_name == "ps":
+        server.join()
+    elif job_name == "worker":
         train(server, cluster)
+    else:
+        raise ValueError("Unknow job name.")
 
 def train(server, cluster):
     print("Training model...")
 
     # config
-    #max_step = 1000
+    max_step = 1000000
     task_count = len(cluster._cluster_spec['worker'])
-    worker_count = len(cluster._cluster_spec['worker'])
-    ps_count = len(cluster._cluster_spec['ps'])
     task_index = FLAGS.task_index
     batch_size = FLAGS.batch_size
     learning_rate = FLAGS.learning_rate
@@ -95,7 +78,7 @@ def train(server, cluster):
         lr = tf.placeholder(tf.float32, name='learning_rate')
         optimizer = tf.train.AdamOptimizer(lr, name='AdamOptimizer')
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        global_step = tf.Variable(0, name="global_step", trainable=False)
+        global_step = tf.train.get_or_create_global_step()
         with tf.control_dependencies(update_ops):
             train_op = optimizer.minimize(loss, global_step=global_step)
 
@@ -106,14 +89,8 @@ def train(server, cluster):
         # merge all summaries into a single "operation"
         # which we can execute in a session
         summary_op = tf.summary.merge_all()
-        init_op = tf.global_variables_initializer()
-        saver = tf.train.Saver(sharded=True)
+        #init_op = tf.global_variables_initializer()
         print("Variables initialized ...")
-
-        enqueue_ops = []
-        for q in create_done_queues(ps_count, worker_count):
-            qop = q.enqueue(1)
-            enqueue_ops.append(qop)
 
     batch_count = int(noisy_data.shape[0] / batch_size)
     print("Batch count %d, Task count %d" % (batch_count, task_count))
@@ -124,95 +101,50 @@ def train(server, cluster):
     else:
         print("Worker %d: Waiting for session to be initialized..." % task_index)
 
-    sv = tf.train.Supervisor(is_chief=(task_index == 0),
-        logdir=ckpt_dir, init_op=init_op, summary_op=None, saver=saver,
-        global_step=global_step, save_model_secs=600)
+    # The StopAtStepHook handles stopping after running given steps.
+    hooks = [tf.train.StopAtStepHook(last_step=max_step)]
 
-    # The supervisor takes care of session initialization, restoring from
-    # a checkpoint, and closing when done or an error occurs.
-
-    #begin_time = time.time()
-    with sv.prepare_or_wait_for_session(server.target) as sess:
-
-        print("Worker %d: Session initialization complete." % task_index)
-
-        # Loop until the supervisor shuts down or n steps have completed.
-        #step = 0
-        #while not sv.should_stop() and step < max_step:
-        # Run a training step asynchronously.
-        # See `tf.train.SyncReplicasOptimizer` for additional details
-        # on how to perform *synchronous* training.
+    # The MonitoredTrainingSession takes care of session initialization,
+    # restoring from a checkpoint, saving to a checkpoint, and closing
+    # when done or an error occurs.
+    with tf.train.MonitoredTrainingSession(master=server.target,
+                                           is_chief=(task_index == 0),
+                                           checkpoint_dir=ckpt_dir,
+                                           hooks=hooks) as mon_sess:
 
         # create log writer object (this will log on every machine)
         print("Save tensorboard files into: {}".format(log_dir))
-        writer = tf.summary.FileWriter(log_dir, sess.graph)
+        writer = tf.summary.FileWriter(log_dir, mon_sess.graph)
         # writer = tf.summary.FileWriter(log_dir, graph=tf.get_default_graph())
 
-        # builder = tf.saved_model.builder.SavedModelBuilder(ckpt_dir)
-        # builder.add_meta_graph_and_variables(sess,
-        #     [tf.saved_model.tag_constants.SERVING],
-        #     signature_def_map = {"magic_model":
-        #     tf.saved_model.signature_def_utils.predict_signature_def(
-        #     inputs= {"image": X}, outputs= {"prediction": Y})},
-        #     clear_devices=True)
-        # RuntimeError: Graph is finalized and cannot be modified.
+        while not mon_sess.should_stop():
+            start_time = time.time()
+            for epoch in range(epoch_count):
+                # Separate data batches to each worker
+                for batch_index in range(0, batch_count, task_count):
+                    current_batch = batch_index + task_index
+                    if current_batch >= batch_count:
+                        current_batch -= batch_count # wrap around
+                    index1 = batch_size * current_batch
+                    index2 = batch_size + index1
+                    noisy_batch = noisy_data[index1:index2, :, :, :]
+                    clean_batch = clean_data[index1:index2, :, :, :]
 
-        # perform training cycles
-        start_time = time.time()
-        for epoch in range(epoch_count):
-            # Separate data batches to each worker
-            for batch_index in range(0, batch_count, task_count):
-                current_batch = batch_index + task_index
-                if current_batch >= batch_count:
-                    current_batch -= batch_count # wrap around
-                index1 = batch_size * current_batch
-                index2 = batch_size + index1
-                noisy_batch = noisy_data[index1:index2, :, :, :]
-                clean_batch = clean_data[index1:index2, :, :, :]
+                    _, loss_value, summary, step = mon_sess.run(
+                        [train_op, loss, summary_op, global_step],
+                        feed_dict={X: noisy_batch, Y_: clean_batch,
+                                   lr: learning_rate, is_training: True})
+                    writer.add_summary(summary, step)
 
-                _, loss_value, summary, step = sess.run(
-                    [train_op, loss, summary_op, global_step],
-                    feed_dict={X: noisy_batch, Y_: clean_batch,
-                               lr: learning_rate, is_training: True})
-                writer.add_summary(summary, step)
+                    elapsed_time = int((time.time() - start_time) / 60)
+                    start_time = time.time()
+                    print("Step: %4d," % (step+1),
+                          "Epoch: %4d," % (epoch+1),
+                          "Task: %4d of %4d," % (task_index+1, task_count),
+                          "Batch: %4d of %4d," % (current_batch+1, batch_count),
+                          "Time (min): %d," % elapsed_time,
+                          "Loss: %.6f," % loss_value)
 
-                elapsed_time = int((time.time() - start_time) / 60)
-                start_time = time.time()
-                print("Step: %4d," % (step+1),
-                      "Epoch: %4d," % (epoch+1),
-                      "Task: %4d of %4d," % (task_index+1, task_count),
-                      "Batch: %4d of %4d," % (current_batch+1, batch_count),
-                      "Time (min): %d," % elapsed_time,
-                      "Loss: %.6f," % loss_value)
-
-            # if np.mod(epoch + 1, 2) == 0:
-            #     saver = tf.train.Saver()
-            #     model_name = 'DnCNN-tensorflow'
-            #     path = os.path.join(ckpt_dir, model_name)
-            #     saver.save(sess, path, global_step=step)
-
-        # print("Test-Accuracy: %2.2f" % sess.run(accuracy,
-        #     feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-        # print("Total Time: %3.2fs" % float(time.time() - begin_time))
-        # print("Final Cost: %.4f" % cost)
-
-        # model_exporter = exporter.Exporter(saver)
-        # model_exporter.init(tf.get_default_graph().as_graph_def(),
-        #     named_graph_signatures={
-        #     'inputs': exporter.generic_signature({'input': X}),
-        #     'outputs': exporter.generic_signature({'output': Y})},
-        #     clear_devices=True)
-        # export_path = ckpt_dir
-        # model_exporter.export(export_path, sess)
-
-        # signal to ps shards that we are done
-        #for q in create_done_queues(ps_count, worker_count):
-        #    sess.run(q.enqueue(1))
-        for op in enqueue_ops:
-            sess.run(op)
-
-    # builder.save()
-    sv.stop()
     print("Done")
 
 
